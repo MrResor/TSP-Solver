7346d80cb4086a76a7f5250d5403b8c3f31fd823
Robert Kwiatkowski <jakusienko@gmail.com>
2022-07-08 22:16:03 +0200

    added comments

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import sys
 
 class Application:
     """Class responsible for PyQt initialization and exiting the program when it is finished
-    
+
         Atributes:
         app     -- holds application, for which we will present windows
         win     -- holds main window and it's contents
     """
     def __init__(self, argv):
+        # runs QtApp
         self.app = QApplication(argv)
         self.win = MainWindow()
         self.win.show()
