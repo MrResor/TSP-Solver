1dd60dd7ec59032bfe1fd37763e809796fe31b0a
Robert Kwiatkowski <jakusienko@gmail.com>
2022-07-12 12:05:28 +0200

    changed to conform to pep8

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from main_window_class import MainWindow
-from __init__ import *
+from __init__ import Qtw
 import sys
 
-class Application:
-    """Class responsible for PyQt initialization and exiting the program when it is finished
 
-        Atributes:
-        app     -- holds application, for which we will present windows
-        win     -- holds main window and it's contents
+class Application:
+    """ Class responsible for PyQt initialization and exiting the program when
+        it is finished\n
+
+        Atributes:\n
+        app -- holds application, for which we will present windows\n
+        win -- holds main window and it's contents\n
     """
-    def __init__(self, argv):
-        # runs QtApp
-        self.app = QApplication(argv)
+    def __init__(self, argv: list) -> None:
+        """ Initializer for Application class\n
+            takes argv (sys.argv) as parameters, sets up and runs Qt app.
+        """
+        self.app = Qtw.QApplication(argv)
         self.win = MainWindow()
         self.win.show()
         sys.exit(self.app.exec_())
