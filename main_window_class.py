bee0854d7fa50eec1e7e8c0fc733d56402c7456b
Robert Kwiatkowski <jakusienko@gmail.com>
2022-10-18 21:22:26 +0200

    removed error handling to find an error that appeared after changning the database

--- main_window_class.py
+++ main_window_class.py
@@ -4,12 +4,21 @@ from solve import solveWindow
 
 
 class MainWindow(Qtw.QMainWindow):
+    """ Class for displaying main window and it's components.\n
+
+        Attributes:\n
+        progress_signal -- signal for handling any errors
+        
+
+    """
+    progress_signal = Qtc.pyqtSignal(int)
 
     def __init__(self, parent=None):
         # setting up windows title for the whole class
         super().__init__(parent)
         self.toVisit = []
         self.setWindowTitle('Route Planning Support')
+        self.progress_signal.connect(self.handle_err_signal)
         self.initUI()
 
     def initUI(self):
@@ -17,24 +26,24 @@ class MainWindow(Qtw.QMainWindow):
         mainwindow = Qtw.QWidget()
 
         hlayout = Qtw.QHBoxLayout()
-        vlayout = self.listWidget()
 
+        vlayout = self.list_widget()
         hlayout.addLayout(vlayout)
 
-        vlayout = self.mapWidget()
+        vlayout = self.map_widget()
 
         hlayout.addLayout(vlayout)
 
         mainwindow.setLayout(hlayout)
         self.setCentralWidget(mainwindow)
 
-    def listWidget(self):
+    def list_widget(self):
         # creating vertical layout with cityList
         vlayout = Qtw.QVBoxLayout()
-
         self.cityList = Qtw.QListWidget()
         self.cityList.setMaximumWidth(600)
-        self.populateCityList()
+        if (self.populateCityList()) == 0:
+            return 0
         self.cityList.itemChanged.connect(self.replot)
         self.cityList.itemActivated.connect(self.mark)
         vlayout.addWidget(self.cityList)
@@ -48,36 +57,40 @@ class MainWindow(Qtw.QMainWindow):
             dlg = ErrDialog("Database file missing!\nIt is neccessary for this\
                             application to work.", 2)
         cur = con.cursor()
-        try:
-            cur.execute('PRAGMA TABLE_INFO(cities)')
-            temp = cur.fetchall()
-            if len(temp) != 5 or temp[0][1] != "id" or temp[0][2] != 'INTEGER' or temp[1][1] != 'node_id' \
-                    or temp[1][2] != 'INTEGER' or temp[2][1] != "name" or temp[2][2] != 'TEXT' or temp[3][1] != 'lat' \
-                    or temp[3][2] != 'REAL' or temp[4][1] != 'lon' or temp[4][2] != 'REAL':
-                con.close()
-                self.progressSignal.emit(2)
-                return 0
-            cur.execute('SELECT * FROM cities ORDER BY name')
-            self.cities = cur.fetchall()
-            cur.execute('SELECT max(id_to) FROM distance')
-            size = cur.fetchone()[0]
-            if size != len(self.cities):
-                con.close()
-                self.progressSignal.emit(2)
-                return 0
-            cur.execute('PRAGMA TABLE_INFO(distance)')
-            temp = cur.fetchall()
-            if len(temp) != 3 or temp[0][1] != 'id_from' or temp[0][2] != "INTEGER" or temp[1][1] != 'id_to' \
-                    or temp[1][2] != "INTEGER" or temp[2][1] != 'distance' or temp[2][2] != "REAL":
-                con.close()
-                self.progressSignal.emit(2)
-                return 0
-        except sqlite3.OperationalError:
-            # if there is no table cities in db, it means it is not mine db,
-            # so we close connection, remove the file we just created and show error dialog
-            con.close()
-            dlg = ErrDialog("Incorrect Database file found!\nPlease use\
-                correct one, provided by creator.", 2)
+        # try:
+        #     cur.execute('PRAGMA TABLE_INFO(cities)')
+        #     temp = cur.fetchall()
+        #     if len(temp) != 5 or temp[0][1] != "id" or temp[0][2] != 'INTEGER' or temp[1][1] != 'node_id' \
+        #             or temp[1][2] != 'INTEGER' or temp[2][1] != "name" or temp[2][2] != 'TEXT' or temp[3][1] != 'lat' \
+        #             or temp[3][2] != 'REAL' or temp[4][1] != 'lon' or temp[4][2] != 'REAL':
+        #         con.close()
+        #         self.progressSignal.emit(2)
+        #         return 0
+        #     cur.execute('SELECT * FROM cities ORDER BY name')
+        cur.execute('SELECT * FROM Cities')
+        #     self.cities = cur.fetchall()
+        self.cities = cur.fetchall()
+        #     cur.execute('SELECT max(id_to) FROM distance')
+        cur.execute('SELECT max(id_to) from Distance')
+        #     size = cur.fetchone()[0]
+        size = cur.fetchone()[0]
+        #     if size != len(self.cities):
+        #         con.close()
+        #         self.progressSignal.emit(2)
+        #         return 0
+        #     cur.execute('PRAGMA TABLE_INFO(distance)')
+        #     temp = cur.fetchall()
+        #     if len(temp) != 3 or temp[0][1] != 'id_from' or temp[0][2] != "INTEGER" or temp[1][1] != 'id_to' \
+        #             or temp[1][2] != "INTEGER" or temp[2][1] != 'distance' or temp[2][2] != "REAL":
+        #         con.close()
+        #         self.progressSignal.emit(2)
+        #         return 0
+        # except sqlite3.OperationalError:
+        #     # if there is no table cities in db, it means it is not mine db,
+        #     # so we close connection, remove the file we just created and show error dialog
+        #     self.progressSignal.emit(2)
+        #     con.close()
+        #     return 0
         con.close()
         self.rows = []
         r = 1
@@ -85,7 +98,7 @@ class MainWindow(Qtw.QMainWindow):
         for row in self.cities:
             rlay = Qtw.QListWidgetItem()
             rlay.setCheckState(Qtc.Qt.Unchecked)
-            rlay.setText(row[2] + " (" + str(r) + ")")
+            rlay.setText(row[1] + " (" + str(r) + ")")
             rlay.setFont(Qtg.QFont('Arial', 16))
             self.rows.append(rlay)
             self.cityList.insertItem(r, rlay)
@@ -96,14 +109,14 @@ class MainWindow(Qtw.QMainWindow):
         self.maptile.axes.cla()
         for r, row in enumerate(self.rows):
             if row.checkState() == 2:
-                self.maptile.axes.plot(self.cities[r][4], self.cities[r][3],
+                self.maptile.axes.plot(self.cities[r][3], self.cities[r][2],
                                        marker='o', color='r')
-                self.maptile.axes.text(self.cities[r][4], self.cities[r][3],
+                self.maptile.axes.text(self.cities[r][3], self.cities[r][2],
                                        r+1, fontsize=10)
                 if self.cities[r] not in self.toVisit:
                     self.toVisit.append(self.cities[r])
             else:
-                self.maptile.axes.plot(self.cities[r][4], self.cities[r][3],
+                self.maptile.axes.plot(self.cities[r][3], self.cities[r][2],
                                        marker='o', color='b')
                 if self.cities[r] in self.toVisit:
                     self.toVisit.remove(self.cities[r])
@@ -112,9 +125,10 @@ class MainWindow(Qtw.QMainWindow):
     def mark(self):
         # function to mark unmarked item and unmark marked one
         var = self.cityList.item(self.cityList.currentRow())
-        var.setCheckState(Qtc.Qt.Checked if var.checkState() == 0 else Qtc.Qt.Unchecked)
+        var.setCheckState(Qtc.Qt.Checked if var.checkState()
+                          == 0 else Qtc.Qt.Unchecked)
 
-    def mapWidget(self):
+    def map_widget(self):
         # creating vertical layout with map, button and label
         vlayout = Qtw.QVBoxLayout()
 
@@ -154,7 +168,11 @@ class MainWindow(Qtw.QMainWindow):
             self.win = solveWindow(self.cities, self.toVisit)
             self.button.setText("Working...")
 
-    def Activate_button(self):
+    def activate_button(self):
         self.button.setEnabled(True)
-    # def dbDataLoad(self):
-    # function to load data from db and check it's contents correctness
+
+    def handle_err_signal(self, code: int) -> None:
+        dlg = ErrDialog("Incorrect Database file found!\nPlease use\
+                correct one, provided by creator.", code)
+        # def dbDataLoad(self):
+        # function to load data from db and check it's contents correctness
