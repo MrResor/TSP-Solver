65bc96d971458c5788ddc66b66c2bc0c59de33b2
Robert Kwiatkowski <jakusienko@gmail.com>
2022-10-18 20:31:01 +0200

    added extra newline

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class Application:
         app -- holds application, for which we will present windows\n
         win -- holds main window and it's contents\n
     """
+
     def __init__(self, argv: list) -> None:
         """ Initializer for Application class\n
             takes argv (sys.argv) as parameters, sets up and runs Qt app.
