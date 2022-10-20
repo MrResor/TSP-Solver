bad8fa20cf07bcaa914242f851be01d476b4f20c
Robert Kwiatkowski <jakusienko@gmail.com>
2022-10-20 18:49:45 +0200

    added decorator for missing tables in db and created signal to notify user

--- decorators.py
+++ decorators.py
@@ -1,3 +1,6 @@
+from __init__ import sqlite3
+
+
 def db_con(func):
     def wrapper_catch_db_error(instance):
         try:
@@ -6,3 +9,13 @@ def db_con(func):
             instance.error_signal.error_signal.emit(1)
 
     return wrapper_catch_db_error
+
+
+def db_querry(func):
+    def wrapper_catch_querry_error(instance, querry):
+        try:
+            return func(instance, querry)
+        except sqlite3.OperationalError:
+            instance.error_signal.error_signal.emit(2)
+
+    return wrapper_catch_querry_error
