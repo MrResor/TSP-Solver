c76bc749e02cab27b093fe6c0165f3264ee2233d
Robert Kwiatkowski <jakusienko@gmail.com>
2022-10-23 21:12:24 +0200

    massive simplifications to algorithms, rewriting to more pythonic code

--- solve.py
+++ solve.py
@@ -1,94 +1,74 @@
-from __init__ import Qtw, Qtc, path, sqlite3, randrange, random
+from __init__ import Qtw, Qtc, random
 from auxilary import ErrDialog, ResDialog
 from threading import Thread
 import pandas as pd
+from numpy import full, Inf, fill_diagonal, ndarray, random, power
+from decorators import data_check
 
 
 class solveWindow(Qtw.QWidget):
     # class for solving real TSP problem with cities given by user
-    progressSignal = Qtc.pyqtSignal(int)
+    progress_signal = Qtc.pyqtSignal()
+    error_signal = Qtc.pyqtSignal(int)
     button_control = Qtc.pyqtSignal()
 
-    def __init__(self, verification, toVisit):
-        # constructor of solveWindow class
+    def __init__(self, db, to_visit):
+        """ Sets up solution class and some result presentation.
+        """
         super().__init__()
         self.setWindowFlags(Qtc.Qt.WindowTitleHint)
         self.setWindowTitle("Working...")
-        self.verification = verification
-        self.toVisit = toVisit
-        self.progressSignal.connect(self.results)
+        self.to_visit = to_visit
+        self.db = db
+        self.progress_signal.connect(self.results)
         self.layout = Qtw.QVBoxLayout()
         self.message = Qtw.QLabel("Calculating Shortest path\nPlease wait...")
         self.message.setAlignment(Qtc.Qt.AlignCenter)
         self.layout.addWidget(self.message)
         self.setLayout(self.layout)
         self.show()
-        t = Thread(target=lambda: self.algorithm(toVisit))
+        t = Thread(target=lambda: self.algorithm())
         t.start()
 
-    def results(self, value: int) -> None:
+    def results(self) -> None:
         # function for displaying dialog with results
         self.hide()
-        if value:
-            if value == 1:
-                self.dlg = ErrDialog("Database file missing!\nIt is neccessary\
-                   for this application to work.", 2)
-            else:
-                self.dlg = ErrDialog("Incorrect Database file found!\nPlease\
-                    use correct one, provided by creator.", 2)
-        else:
-            self.dlg = ResDialog(self.response)
-            self.dlg.button_control.connect(self.activate_button)
-            self.dlg.exec_()
+        self.dlg = ResDialog(self.response)
+        self.dlg.button_control.connect(self.activate_button)
+        self.dlg.exec_()
         self.close()
 
-    def algorithm(self, toVisit):
+    def algorithm(self):
         # full algorithm, we generate non existing paths between cities from real ones using floydWarshall algorithm
         # only lengths, there is no record of what cities were used
         dist_full = self.floydWarshall()
-        if dist_full == 0:
-            return 0
+        if type(dist_full) != ndarray:
+            return
         # with full 72 x 72 table of distances we cut unnecessary ones and pass that distance table into ants function
-        size = len(toVisit)
-        dist = []
-        for i in range(size):
-            row = []
-            for j in range(size):
-                row.append(0)
-            dist.append(row)
-        for i in range(size):
-            for j in range(i, size):
-                dist[i][j] = dist_full[toVisit[i][0] - 1][toVisit[j][0] - 1]
-                dist[j][i] = dist_full[toVisit[j][0] - 1][toVisit[i][0] - 1]
-        order = self.ants(dist, size)
-        self.response = []
-        start = order.index(0)
+        self.size = len(self.to_visit)
+        self.dist = full((self.size, self.size), 0.0)
+        for i in range(self.size):
+            for j in range(i, self.size):
+                self.dist[i][j] = dist_full[self.to_visit[i]
+                                            [0] - 1][self.to_visit[j][0] - 1]
+                self.dist[j][i] = dist_full[self.to_visit[j]
+                                            [0] - 1][self.to_visit[i][0] - 1]
+        order = self.ants()
+        start = list(order).index(0)
         # prepairing correct order for data that will be displayed
         # (first city that was marked on the list is our first city on the output)
-        for i in range(size):
-            self.response.append(toVisit[order[(start + i) % size]][1:])
+        self.response = [
+            self.to_visit[order[(start + i) % self.size]][1:] for i in range(self.size)]
         # emmiting a signal that algorithm has finished it's work
-        self.progressSignal.emit(0)
+        self.progress_signal.emit()
 
+    @data_check
     def floydWarshall(self):
         # floyd warshall algorithm to make up for not existing connections between cities (needed for TSP)
-        if path.exists("database.db"):
-            con = sqlite3.connect("database.db")
-        else:
-            self.progressSignal.emit(1)
-            return 0
-        cur = con.cursor()
-        cur.execute('SELECT max(id_to) FROM Distance')
-        size = cur.fetchone()[0]
-        cur.execute('SELECT * FROM Distance')
-        val = cur.fetchall()
-        con.close()
-        dist = []
-        for i in range(size):
-            row = []
-            for j in range(size):
-                row.append(1.0e+200 * (i != j))
-            dist.append(row)
+        size = self.db.querry('SELECT max(id_to) FROM Distance')[0][0]
+        val = self.db.querry('SELECT * FROM Distance')
+        dist = full((size, size), Inf)
+        fill_diagonal(dist, 0)
         for i in val:
             dist[i[0]-1][i[1]-1] = i[2]
             dist[i[1]-1][i[0]-1] = i[2]
@@ -97,105 +77,83 @@ class solveWindow(Qtw.QWidget):
                 for j in range(size):
                     if dist[i][j] > dist[i][k] + dist[k][j]:
                         dist[i][j] = dist[i][k] + dist[k][j]
-        return dist
+        if (dist == Inf).any():
+            raise RuntimeError
+        else:
+            return dist
 
-    def ants(self, dist, size):
+    def ants(self):
         # ant colony algorithm
-        best = self.antsTables(dist, size)
+        best = self.antsTables()
         for trip in range(40):
-            self.antAnda(size)
-            self.antsTraveling(size, dist)
-            min = 1e+200
+            self.antAnda()
+            self.antsTraveling()
+            min = Inf
             index = 0
-            for i in range(size):
-                if self.ant[i][size] < min:
+            for i in range(self.size):
+                if self.ant[i][self.size] < min:
                     index = i
-                    min = self.ant[i][size]
-            if best[size] > self.ant[index][size]:
+                    min = self.ant[i][self.size]
+            if best[self.size] > self.ant[index][self.size]:
                 best = self.ant[index]
-            self.pheromones(size)
+            self.pheromones()
         return best
 
-    def antsTables(self, dist, size):
+    def antsTables(self):
         # fucntion for creating necessary tables
-        self.tau = []
-        self.a = []
-        self.p = []
-        self.n = []
-        best = []
-        longest = pd.DataFrame(dist).max().max()
-        for i in range(size):
-            rowtau = []
-            rowa = []
-            rown = []
-            for j in range(size):
-                rowtau.append(1/longest)
-                rowa.append(0)
-                rown.append(1/dist[i][j] if dist[i][j] != 0 else 0)
-            self.tau.append(rowtau)
-            self.a.append(rowa)
-            self.n.append(rown)
-            self.p.append(0)
-            best.append(0)
-        best.append(1e+200)
+        self.tau = full((self.size, self.size), 1/self.dist.max())
+        self.a = full((self.size, self.size), 0.0)
+        self.n = 1/self.dist
+        fill_diagonal(self.n, 0)
+        best = full(self.size + 1, 0.0)
+        best[self.size] = Inf
         return best
 
-    def antAnda(self, size):
+    def antAnda(self):
         # creating anew ant table and filling a
         alpha = 1
         beta = 2
-        self.ant = []
-        self.start = []
-        for i in range(size):
-            row = []
-            for j in range(size):
-                self.a[i][j] = (self.tau[i][j] ** alpha) * \
-                    (self.n[i][j] ** beta)
-                row.append(size + 1)
-            row.append(0)
-            self.ant.append(row)
-            self.start.append(randrange(size))
-            s = sum(self.a[i])
-            self.a[i] = [val / s for val in self.a[i]]
+        self.ant = full((self.size, self.size + 1), self.size + 1)
+        self.ant[:, self.size] = 0
+        self.a = power(self.tau, alpha) * power(self.n, beta)
+        self.start = random.randint(0, self.size, self.size)
+        self.a = [val / sum(val) for val in self.a]
 
-    def antsTraveling(self, size: int, dist) -> None:
+    def antsTraveling(self) -> None:
         # function that simulates "travelling" of ants
-        for i in range(size):
-            self.ant[i][0] = self.start[i]
-            for j in range(1, size):
-                for q in range(size):
-                    self.p[q] = self.a[self.ant[i][j-1]][q] * \
-                        (q not in self.ant[i])
-                s = sum(self.p)
-                self.p = [val / s for val in self.p]
+        self.ant[:, 0] = self.start
+        for i in range(self.size):
+            for j in range(1, self.size):
+                self.p = [self.a[self.ant[i, j-1]][q] *
+                          (q not in self.ant[i]) for q in range(self.size)]
+                self.p = [val / sum(self.p) for val in self.p]
                 r = 0
                 while r == 0:
-                    r = random()
-                for q in range(size):
+                    r = random.rand(1)
+                for q in range(self.size):
                     r -= self.p[q]
                     if r <= 0:
                         self.ant[i][j] = q
-                        self.ant[i][size] += dist[self.ant[i]
-                                                  [j - 1]][self.ant[i][j]]
+                        self.ant[i][self.size] += self.dist[self.ant[i]
+                                                            [j - 1]][self.ant[i][j]]
                         break
-            self.ant[i][size] += dist[self.ant[i][size-1]][self.ant[i][0]]
+            self.ant[i][self.size] += self.dist[self.ant[i]
+                                                [self.size-1]][self.ant[i][0]]
 
-    def pheromones(self, size: int) -> None:
+    def pheromones(self) -> None:
         # function for updating pheromones on each arc
         rho = 0.5
-        for i in range(size):
-            for j in range(size):
-                self.tau[i][j] *= (1-rho)
-        for i in range(size):
-            for j in range(1, size):
+        self.tau *= (1-rho)
+        for i in range(self.size):
+            for j in range(1, self.size):
                 self.tau[self.ant[i][j - 1]][self.ant[i]
-                                             [j]] += 1/self.ant[i][size]
+                                             [j]] += 1/self.ant[i][self.size]
                 self.tau[self.ant[i][j]][self.ant[i]
-                                         [j - 1]] += 1/self.ant[i][size]
+                                         [j - 1]] += 1/self.ant[i][self.size]
             self.tau[self.ant[i][0]][self.ant[i]
-                                     [size - 1]] += 1/self.ant[i][size]
-            self.tau[self.ant[i][size - 1]][self.ant[i]
-                                            [0]] += 1/self.ant[i][size]
+                                     [self.size - 1]] += 1/self.ant[i][self.size]
+            self.tau[self.ant[i][self.size - 1]][self.ant[i]
+                                                 [0]] += 1/self.ant[i][self.size]
 
     def activate_button(self):
         self.button_control.emit()
